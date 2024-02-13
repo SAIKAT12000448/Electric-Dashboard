87eb512d88e6096d61aea9957c8a194d9c4fc2e9
SAIKAT12000448 <73012048+SAIKAT12000448@users.noreply.github.com>
2024-02-13 20:29:53 +0600

    ssss

--- 21days/project1/electric-gadget-server/index.js
+++ 21days/project1/electric-gadget-server/index.js
@@ -9,6 +9,14 @@ const cors = require('cors');
 
 // app.use(cors());
 app.use(cors({ origin: '*' }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://electric-gadget-icvzqa7n2-saikat12000448.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 
 app.use(express.json());
 
