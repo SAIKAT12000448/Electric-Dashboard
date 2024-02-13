cf82e1cb00f545fe2b7820921b8d5ee994e387fc
SAIKAT12000448 <73012048+SAIKAT12000448@users.noreply.github.com>
2024-02-13 09:14:47 +0600

    sss

--- 21days/project1/electric-gadget-server/index.js
+++ 21days/project1/electric-gadget-server/index.js
@@ -7,7 +7,9 @@ const port = process.env.PORT || 5000;
 
 const cors = require('cors');
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({ origin: '*' }));
+
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lrc63.mongodb.net/?retryWrites=true&w=majority`;
